bc8b1a70acce58204c24f3d72449cfc374d69def
sunny-64 <sunny6464n@gmail.com>
2025-02-22 14:53:11 +0530

    temp commit

--- src/components/core/toggleButton/index.tsx
+++ src/components/core/toggleButton/index.tsx
@@ -1,4 +1,4 @@
-import { Switch } from '@headlessui/react';
+import { Switch } from "@/components/ui/switch"
 
 interface ToggleButtonProps {
   label: string;
--- src/layouts/Root.tsx
+++ src/layouts/Root.tsx
@@ -3,8 +3,8 @@ import { Navbar } from '../components';
 
 const Root = () => {
   return (
-    <div className='min-h-screen px-10 py-4'>
-      <Navbar />
+    <div className="min-h-screen px-4 md:px-10 py-4">
+      {/* <Navbar /> */}
       <Outlet />
     </div>
   );
