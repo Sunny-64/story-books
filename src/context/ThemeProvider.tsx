313d12dbcf4f629575e61b52b68c1a209c0d5fff
sunny-64 <sunny6464n@gmail.com>
2025-02-22 14:44:51 +0530

    shadcn setup

--- src/context/ThemeProvider.tsx
+++ src/context/ThemeProvider.tsx
@@ -1,27 +1,50 @@
-import { createContext, useEffect, useState } from 'react';
-import { getLocalItem, setLocalItem } from '../utils';
+import { createContext, useState, useEffect } from 'react';
 
-export interface IThemeContext {
-  theme: string;
-  toggleTheme: () => void;
-}
+type Theme = 'dark' | 'light' | 'system';
 
-export const ThemeContext = createContext<IThemeContext | null>(null);
-
-const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
-  const [theme, setTheme] = useState<string>(getLocalItem('theme') ?? 'light');
-
-  const toggleTheme = () => {
-    const newTheme = theme === 'light' ? 'dark' : 'light';
-    setTheme(newTheme);
-    setLocalItem('theme', JSON.stringify(newTheme));
-  };
-
-  useEffect(() => {
-    document.getElementById("root")!.className = `${theme}-mode`;
-  }, [theme]);
-
-  return <ThemeContext.Provider value={{ theme, toggleTheme }}>{children}</ThemeContext.Provider>;
+type ThemeProviderProps = {
+  children: React.ReactNode;
+  defaultTheme?: Theme;
+  storageKey?: string;
 };
 
-export default ThemeProvider;
+type ThemeProviderState = {
+  theme: Theme;
+  setTheme: (theme: Theme) => void;
+};
+
+const initialState: ThemeProviderState = {
+  theme: 'system',
+  setTheme: () => null,
+};
+
+export const ThemeContext = createContext<ThemeProviderState>(initialState);
+
+export function ThemeProvider({ children, defaultTheme = 'system', storageKey = 'vite-ui-theme' }: ThemeProviderProps) {
+  const [theme, setTheme] = useState<Theme>(() => (localStorage.getItem(storageKey) as Theme) || defaultTheme);
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+
+    root.classList.remove('light', 'dark');
+
+    if (theme === 'system') {
+      const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+
+      root.classList.add(systemTheme);
+      return;
+    }
+
+    root.classList.add(theme);
+  }, [theme]);
+
+  const value = {
+    theme,
+    setTheme: (theme: Theme) => {
+      localStorage.setItem(storageKey, theme);
+      setTheme(theme);
+    },
+  };
+
+  return <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>;
+}
